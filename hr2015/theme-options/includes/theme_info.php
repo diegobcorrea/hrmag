da1a599c81b1cf20e3fefe393fbf8532a9a7949d
Diego B. Correa <ddumst@gmail.com>
2015-02-10 11:58:17 -0500

    Changes on new server

--- hr2015/theme-options/includes/theme_info.php
+++ hr2015/theme-options/includes/theme_info.php
@@ -1,5 +1,5 @@
 <?php
-$reedwan_theme_data = get_theme_data(get_template_directory() . '/style.css');
+$reedwan_theme_data = wp_get_theme(get_template_directory() . '/style.css');
 define('THEME_NAME', $reedwan_theme_data['Name']);
 define('THEME_AUTHOR', $reedwan_theme_data['Author']);
 define('THEME_VERSION', trim($reedwan_theme_data['Version']));
